c4c7d90216d6baa2bfe83f55358918551bf8693f
Gustavo Ruiz <you@example.com>
2023-06-02 09:17:05 -0300

    front-end file cleaning

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
 import Layout from "./components/Layout";
-import Admin from "./components/Admin";
-import Missing from "./components/Missing";
-import Unauthorized from "./components/Unauthorized";
-import LinkPage from "./components/LinkPage/LinkPage";
-import RequireAuth from "./components/RequireAuth";
+import LinkPage from "./components/Home/Home";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -17,20 +12,7 @@ function App() {
 				{/* public routes */}
 				<Route path="login" element={<Login />} />
 				<Route path="register" element={<Register />} />
-				<Route path="linkpage" element={<LinkPage />} />
-				<Route path="unauthorized" element={<Unauthorized />} />
-
-				{/* we want to protect these routes */}
-				<Route element={<RequireAuth/>}>
-					<Route path="/" element={<Home />} />
-				</Route>
-
-				<Route element={<RequireAuth/>}>
-					<Route path="admin" element={<Admin />} />
-				</Route>
-
-				{/* catch all */}
-				<Route path="*" element={<Missing/>}/>
+				<Route path="home" element={<LinkPage />} />
 			</Route>
 		</Routes>
 	);
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,30 +1,45 @@
-import { useNavigate, Link } from "react-router-dom";
-import { useContext } from "react";
-import AuthContext from "../../context/AuthProvider";
+import { Link } from "react-router-dom"
+import './LinkPage.css'
+import axios from 'axios';
+
+const LOGOUT_URL = '/logout';
+
+const handleLogout = async (e) => {
+  e.preventDefault();
+
+  const authData = JSON.parse(localStorage.getItem('authData'));
+  const token = authData?.token;
+  const id = authData?.id;
+
+  if (!token || !id) {
+    console.log('Missing token or id');
+    return;
+  }
+
+  try {
+    const response = await axios.post('/logout',
+    { id: id},
+    {
+        headers: { 'Content-Type': 'application/json', 'authorization': `Bearer ${token}`},
+    }
+    );
+    console.log(id)
+} catch (err){
+    console.log('erro', err);
+}
+};
 
 const Home = () => {
-    const { setAuth } = useContext(AuthContext);
-    const navigate = useNavigate();
-
-    const logout = async () => {
-        setAuth({});
-        navigate('/linkpage');
-    }
-
     return (
-        <section>
-            <h1>Home</h1>
-            <br />
-            <p>You are logged in!</p>
-            <br />
-            <Link to="/admin">Go to the Admin page</Link>
-            <br />
-            <Link to="/linkpage">Go to the link page</Link>
-            <div className="flexGrow">
-                <button onClick={logout}>Sign Out</button>
-            </div>
+        <section className="linkPage_main">
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <button onClick={handleLogout} href="/register">Fazer Logout</button>
+          </p>
         </section>
-    )
-}
+      );
+    };
+
 
 export default Home
