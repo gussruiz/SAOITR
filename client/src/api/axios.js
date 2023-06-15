7beebfd8757bf5cfd46364c801c60b9a51e5b97c
Gustavo Ruiz <you@example.com>
2023-06-15 00:00:47 -0300

    axios fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: 'http://localhost:4444'
+    baseURL: 'http://localhost:4444'
     // baseURL: 'http://10.20.8.195:24000'
     // baseURL: 'http://10.20.8.119:25000'
-    baseURL: 'http://10.20.8.61:22333'
+    // baseURL: 'http://10.20.8.61:22333'
     // baseURL: 'http://10.20.8.112:20000'
     // baseURL: 'http://10.20.8.196:22000'
     // baseURL: 'http://10.20.8.109:23000'
